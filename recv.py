5f90b8e7c1506a5a341443c8d98bb640ad6ef392
mjlee111 <menggu1234@naver.com>
2025-03-11 15:41:31 +0900

    feat: update

--- recv.py
+++ recv.py
@@ -10,7 +10,7 @@ while True:
     frame = udp_receiver.recv_frame()  
     if frame is None:
         continue
-
+    frame = cv2.resize(frame, (1280, 480))
     cv2.imshow("Receiving", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
